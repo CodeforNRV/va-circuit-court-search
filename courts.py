8ca21f4bfa67d4e41a6758df8713bae057a1ffb4
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2015-09-28 10:22:07 -0400

    Sub search civil cases

--- courts.py
+++ courts.py
@@ -8,6 +8,7 @@ import pygal
 import locale
 import re
 import sys
+import flask
 from bson.son import SON
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
@@ -172,6 +173,7 @@ def searchCourt(name, court):
     if cases is not None:
         print 'Found cached search'
         courtSearch['civilCases'] = cases['civilCases']
+        courtSearch['cached'] = True
     else:
         cookieJar = cookielib.CookieJar()
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
@@ -194,27 +196,28 @@ def searchCourt(name, court):
 
     if cases is None:
         print 'Caching search'
-        for civil_case in courtSearch['civilCases']:
-            if civil_case['Plaintiff'].startswith(name.upper()):
-                continue
-            if db['civil_cases'].find_one({'court': court, 'caseNumber': civil_case['caseNumber']}) is not None:
-                continue
-            civil_case['court'] = court
-            civil_case['search_term'] = name.upper()
-            print civil_case['caseNumber']
-            try:
-                get_case_details(opener, civil_case)
-                sleep(1)
-            except:
-                print 'ERROR GETTING CASE DETAILS!', sys.exc_info()
-            db['civil_cases'].insert(civil_case)
+        #for civil_case in courtSearch['civilCases']:
+        #    if civil_case['Plaintiff'].startswith(name.upper()):
+        #        continue
+        #    if db['civil_cases'].find_one({'court': court, 'caseNumber': civil_case['caseNumber']}) is not None:
+        #        continue
+        #    civil_case['court'] = court
+        #    civil_case['search_term'] = name.upper()
+        #    print civil_case['caseNumber']
+        #    try:
+        #        get_case_details(opener, civil_case)
+        #        sleep(1)
+        #    except:
+        #        print 'ERROR GETTING CASE DETAILS!', sys.exc_info()
+        #    db['civil_cases'].insert(civil_case)
         db['cases'].insert({
             'name': name,
             'court': court,
             'civilCases': courtSearch['civilCases'],
             'dateSaved': datetime.utcnow()
         })
-    return render_template('court.html', court=courtSearch)
+    courtSearch['html'] = render_template('court.html', court=courtSearch)
+    return flask.jsonify(**courtSearch)
 
 
 @app.route("/search/<name>")
@@ -236,275 +239,24 @@ def search(name):
         })
     data = {'name': name.upper(), 'courts': courts}
     cases = db['cases'].find({'name': name.upper()})
-    for case in cases:
-        for court in data['courts']:
-            if case['court'] == court['fullName']:
-                court['civilCases'] = case['civilCases']
-                for x in court['civilCases']:
-                    x.pop('_id', None)
+    print cases
+    if cases.count() == 0:
+        db['searches'].insert({'name': name.upper(), 'searchtime': datetime.utcnow()})
+    #for case in cases:
+    #    for court in data['courts']:
+    #        if case['court'] == court['fullName']:
+    #            court['civilCases'] = case['civilCases']
+    #            for x in court['civilCases']:
+    #                x.pop('_id', None)
     return render_template('search.html', data=data)
-
 
 @app.route("/")
 def index():
-    return render_template('index.html')
-
-@app.route("/charges")
-def charges():
-    client = pymongo.MongoClient(os.environ['MONGO_URI'])
-    db = client.va_circuit_court
-    charges = db.criminal_cases.aggregate([
-        {'$group':{
-            '_id': {
-                'CodeSection': '$CodeSection',
-                'Race': '$Race'
-            },
-            'charge': {'$first': '$Charge'},
-            'court': {'$first': '$Court'},
-            'caseNumber': {'$first': '$CaseNumber'},
-            'avgSentence': {'$avg': '$SentenceTimeDays'},
-            'avgSentenceSuspended': {'$avg': '$SentenceSuspendedDays'},
-            'count': {'$sum': 1}
-        }},
-        {'$group':{
-            '_id': {
-                'CodeSection': '$_id.CodeSection'
-            },
-            'races': {'$push': {
-                'race': '$_id.Race',
-                'avgSentence': '$avgSentence',
-                'avgSentenceSuspended': '$avgSentenceSuspended',
-                'count': '$count'
-            }},
-            'count': {'$sum': '$count'},
-            'avgSentence': {'$avg': '$avgSentence'},
-            'avgSentenceSuspended': {'$avg': '$avgSentenceSuspended'},
-            'charge': {'$first': '$charge'},
-            'court': {'$first': '$court'},
-            'caseNumber': {'$first': '$caseNumber'}
-        }},
-        {'$match' : {
-            'count' : {'$gt' : 50}
-        }},
-        {'$sort': SON([
-            ('_id.CodeSection', 1)
-        ])}
-    ])['result']
-
-    charges_amended = db.criminal_cases.aggregate([
-        {'$match': {'AmendedCharge': {'$ne': None}}},
-        {'$group':{
-            '_id': {
-                'CodeSection': '$CodeSection',
-                'Race': '$Race'
-            },
-            'charge': {'$first': '$Charge'},
-            'court': {'$first': '$Court'},
-            'caseNumber': {'$first': '$CaseNumber'},
-            'avgSentence': {'$avg': '$SentenceTimeDays'},
-            'avgSentenceSuspended': {'$avg': '$SentenceSuspendedDays'},
-            'count': {'$sum': 1}
-        }},
-        {'$group':{
-            '_id': {
-                'CodeSection': '$_id.CodeSection'
-            },
-            'races': {'$push': {
-                'race': '$_id.Race',
-                'avgSentence': '$avgSentence',
-                'avgSentenceSuspended': '$avgSentenceSuspended',
-                'count': '$count'
-            }},
-            'count': {'$sum': '$count'},
-            'avgSentence': {'$avg': '$avgSentence'},
-            'avgSentenceSuspended': {'$avg': '$avgSentenceSuspended'},
-            'charge': {'$first': '$charge'},
-            'court': {'$first': '$court'},
-            'caseNumber': {'$first': '$caseNumber'}
-        }},
-        {'$sort': SON([
-            ('_id.CodeSection', 1)
-        ])}
-    ])['result']
-
-    for charge in charges:
-        charge['amended'] = {
-            'count': 0,
-            'avgSentence': 0,
-            'avgSentenceSuspended': 0,
-            'races': []
-        }
-        for charge_amended in charges_amended:
-            if charge_amended['_id']['CodeSection'] == charge['_id']['CodeSection']:
-                charge['amended'] = charge_amended
-                break
-        charge['races_dict'] = {
-            'White Caucasian (Non-Hispanic)': {
-                'count': 0,
-                'avgSentence': 0,
-                'avgSentenceSuspended': 0
-            },
-            'Black (Non-Hispanic)': {
-                'count': 0,
-                'avgSentence': 0,
-                'avgSentenceSuspended': 0
-            }
-        }
-        charge['amended']['races_dict'] = {
-            'White Caucasian (Non-Hispanic)': {
-                'count': 0,
-                'avgSentence': 0,
-                'avgSentenceSuspended': 0
-            },
-            'Black (Non-Hispanic)': {
-                'count': 0,
-                'avgSentence': 0,
-                'avgSentenceSuspended': 0
-            }
-        }
-        for race in charge['races']:
-            if 'race' in race:
-                charge['races_dict'][race['race']] = race
-        for race in charge['amended']['races']:
-            if 'race' in race:
-                charge['amended']['races_dict'][race['race']] = race
-
-    return render_template('charges.html', charges=charges, charges_amended=charges_amended)
-
-@app.route("/opendata")
-def open_data():
-    client = pymongo.MongoClient(os.environ['MONGO_CASES_URI'])
-    db = client.va_circuit_court_cases
-    locale.resetlocale()
-    case_number_count = locale.format("%d", db.case_numbers.count(), grouping=True)
-    data = {
-        'case_number_count': case_number_count
-    }
-    return render_template('open_data.html', data=data)
-
-@app.route("/opendata/progress")
-def open_data_progress():
-    client = pymongo.MongoClient(os.environ['MONGO_CASES_URI'])
-    db = client.va_circuit_court_cases
-    data = db.case_numbers.aggregate([
-        {'$sort': SON([
-            ('court', 1),
-            ('name', 1)
-        ])},
-        {'$group':{
-            '_id': {
-                'court': '$court'
-            },
-            'firstName': {'$first': '$name'},
-            'lastName': {'$last': '$name'},
-            'count': {'$sum': 1}
-        }},
-        {'$sort': SON([
-            ('_id.court', 1)
-        ])}
-    ])['result']
-    chart = pygal.HorizontalBar(style=pygal.style.RedBlueStyle, show_legend=False, height=1500)
-    chart.x_labels = [x['_id']['court'] for x in data][::-1]
-    chart.add('', [x['count'] for x in data][::-1])
-    return chart.render() + str(render_template('open_data_progress.html', data=data))
-
-@app.route("/sampleLetter")
-def sample_letter():
-    return render_template('sample_letter.html')
-
-@app.route("/stats")
-def stats():
-    return render_template('stats.html')
-
-@app.route("/stats/graph", methods=['POST'])
-def graph():
-    categories = request.get_json(force=True)['categories']
-    print categories
-
-    category = categories[0]['category']
-    sub_category = categories[1]['category']
-    sort_by = categories[0]['sort']
-    if sort_by == 'alpha':
-        sort_by = '_id.' + category
-    sort_direction = int(categories[0]['sortDirection'])
-    sort = (sort_by, sort_direction)
-    filters = categories[0]['filter']
-
-    first_group_stage = {'$group':{
-        '_id': {
-            category: '$' + category
-        },
-        'count': {'$sum': 1}
-    }}
-    second_group_stage = None
-    if sub_category != '':
-        first_group_stage['$group']['_id'][sub_category] = '$' + sub_category
-        second_group_stage =  {'$group':{
-            '_id': {
-                category: '$_id.' + category,
-            },
-            'data': {'$push': {
-                sub_category: '$_id.' + sub_category,
-                'count': '$count'
-            }},
-            'count': {'$sum': '$count'}
-        }}
-    sort_stage = {'$sort': SON([
-        sort
-    ])}
-
-    client = pymongo.MongoClient(os.environ['MONGO_URI'])
-    db = client.va_circuit_court
-    data = None
-    if second_group_stage is None:
-        data = db.criminal_cases.aggregate([
-            first_group_stage,
-            sort_stage
-        ])['result']
-    else:
-        data = db.criminal_cases.aggregate([
-            first_group_stage,
-            second_group_stage,
-            sort_stage
-        ])['result']
-
-    sub_category_names = []
-    if sub_category != '':
-        for group in data:
-            for sub_category_group in group['data']:
-                sub_category_name = 'None'
-                if sub_category in sub_category_group:
-                    sub_category_name = sub_category_group[sub_category]
-                if sub_category_name not in sub_category_names:
-                    sub_category_names.append(sub_category_name)
-                group[sub_category_name] = sub_category_group['count']
-    print pprint(data)
-
-    pprint(sub_category_names)
-    values = [str(x['_id'][category]) for x in data]
-    labels = [v for v in values if v not in filters][:20]
-
-    bar_chart = pygal.Bar(height=450, style=LightStyle, x_label_rotation=70)
-    bar_chart.title = 'VA Circuit Court Cases in 2014'
-    bar_chart.x_labels = labels
-    if sub_category == '':
-        bar_chart.add(category, [x['count'] for x in data if str(x['_id'][category]) not in filters][:20])
-    else:
-        for item in sub_category_names[:10]:
-            item_counts = []
-            for x in data:
-                if str(x['_id'][category]) in filters: continue
-                if item in x:
-                    item_counts.append(x[item])
-                else:
-                    item_counts.append(0)
-            bar_chart.add(item, item_counts[:20])
-
-    return str(render_template('stats_filters.html',
-        category=category,
-        filter_values=sorted(values),
-        filters_unchecked=filters)) + \
-        bar_chart.render()
+    db = pymongo.Connection(os.environ['MONGO_URI'])['court-search-temp']
+    prev_searches = list(db['searches'].find())
+    for search in prev_searches:
+        search['courts_searched'] = db['cases'].find({'name': search['name']}).count()
+    return render_template('index.html', searches=prev_searches)
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
