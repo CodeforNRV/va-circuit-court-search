3af11b1ac34ccff4ef35bb8ee977f8414aaee25a
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2015-10-01 14:00:24 -0400

    Add scraper for hospital data

--- hospitals.py
+++ hospitals.py
@@ -18,23 +18,9 @@ from time import sleep
 from bson.objectid import ObjectId
 from werkzeug import Response
 
-app = Flask(__name__)
-
+session = {}
 user_agent = u"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; " + \
     u"rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11"
-
-class MongoJsonEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (datetime.datetime, datetime.date)):
-            return obj.isoformat()
-        elif isinstance(obj, ObjectId):
-            return unicode(obj)
-        return json.JSONEncoder.default(self, obj)
-
-def jsonify(*args, **kwargs):
-    """ jsonify with support for MongoDB ObjectId
-    """
-    return Response(json.dumps(dict(*args, **kwargs), cls=MongoJsonEncoder), mimetype='application/json')
 
 def handle_parse_exception(soup):
     print '\nException parsing HTML.', \
@@ -197,12 +183,12 @@ def lookupCases(opener, name, court, division):
             raise
     return cases
 
-@app.route("/case/<caseNumber>/court/<path:court>")
+#@app.route("/case/<caseNumber>/court/<path:court>")
 def case_details(caseNumber, court):
     if 'cookies' not in session:
         return "Error. Please reload the page."
     courtId = court[:3]
-    db = pymongo.Connection(os.environ['MONGO_URI'])['court-search-temp']
+    db = pymongo.MongoClient(os.environ['MONGO_URI'])['court-search-temp']
     case_details = db['detailed_cases'].find_one({'court': court, 'caseNumber': caseNumber})
     if case_details is not None:
         print 'Found cached search'
@@ -231,7 +217,7 @@ def case_details(caseNumber, court):
         db['detailed_cases'].insert(case_details)
     return jsonify(**case_details)
 
-@app.route("/search/<name>/court/<path:court>")
+#@app.route("/search/<name>/court/<path:court>")
 def searchCourt(name, court):
     if 'cookies' not in session:
         return "Error. Please reload the page."
@@ -239,12 +225,10 @@ def searchCourt(name, court):
     courtId = court[:3]
     courtSearch = {'name': court[5:], 'id': courtId}
 
-    db = pymongo.Connection(os.environ['MONGO_URI'])['court-search-temp']
+    db = pymongo.MongoClient(os.environ['MONGO_URI'])['court-search-temp']
     cases = db['cases'].find_one({'name': name, 'court': court})
     if cases is not None:
         print 'Found cached search'
-        courtSearch['civilCases'] = cases['civilCases']
-        courtSearch['cached'] = True
     else:
         cookieJar = cookielib.CookieJar()
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
@@ -252,33 +236,31 @@ def searchCourt(name, court):
 
         for cookie in pickle.loads(session['cookies']):
             cookieJar.set_cookie(cookie)
-        data = urllib.urlencode({
-            'courtId': courtId,
-            'courtType': 'C',
-            'caseType': 'ALL',
-            'testdos': False,
-            'sessionCreate': 'NEW',
-            'whichsystem': court})
-        place_url = u"http://ewsocis1.courts.state.va.us/CJISWeb/MainMenu.do"
-        opener.open(place_url, data)
-        session['courtId'] = courtId
+        if 'courtId' not in session or session['courtId'] != courtId:
+            print 'Changing court'
+            data = urllib.urlencode({
+                'courtId': courtId,
+                'courtType': 'C',
+                'caseType': 'ALL',
+                'testdos': False,
+                'sessionCreate': 'NEW',
+                'whichsystem': court})
+            place_url = u"http://ewsocis1.courts.state.va.us/CJISWeb/MainMenu.do"
+            opener.open(place_url, data)
+            session['courtId'] = courtId
         courtSearch['civilCases'] = lookupCases(opener, name.upper(),
                                                 courtId, 'CIVIL')
-        print 'Caching search'
+        print 'Caching search', str(len(courtSearch['civilCases'])), 'cases found'
         db['cases'].insert({
             'name': name,
             'court': court,
             'civilCases': courtSearch['civilCases'],
             'dateSaved': datetime.datetime.utcnow()
         })
-    courtSearch['court'] = court
-    courtSearch['name'] = name
-    courtSearch['html'] = render_template('court.html', court=courtSearch)
-    return jsonify(**courtSearch)
 
-@app.route("/search/<name>/courts")
+#@app.route("/search/<name>/courts")
 def searchCourts(name):
-    db = pymongo.Connection(os.environ['MONGO_URI'])['court-search-temp']
+    db = pymongo.MongoClient(os.environ['MONGO_URI'])['court-search-temp']
     result = {
         'name': name,
         'searches': {}
@@ -288,14 +270,16 @@ def searchCourts(name):
         result['searches'][case['court']] = case
     return jsonify(**result)
 
-@app.route("/search/<name>")
-def search(name):
-    db = pymongo.Connection(os.environ['MONGO_URI'])['court-search-temp']
+#@app.route("/search/<name>")
+def start():
     cookieJar = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
     opener.addheaders = [('User-Agent', user_agent)]
     home = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/circuit.jsp')
+    if 'cookies' in session: print 'Old cookie', session['cookies']
     session['cookies'] = pickle.dumps(list(cookieJar))
+    print 'New cookie', session['cookies']
+    session['courtId'] = None
 
     courts = []
     html = BeautifulSoup(home.read())
@@ -305,29 +289,53 @@ def search(name):
             'id': option['value'][:3],
             'name': option['value'][5:]
         })
-    data = {'name': name.upper(), 'courts': courts}
-    cases = db['cases'].find({'name': name.upper()})
-    print cases
-    if cases.count() == 0:
-        db['searches'].insert({'name': name.upper(), 'searchtime': datetime.datetime.utcnow()})
-    #for case in cases:
-    #    for court in data['courts']:
-    #        if case['court'] == court['fullName']:
-    #            court['civilCases'] = case['civilCases']
-    #            for x in court['civilCases']:
-    #                x.pop('_id', None)
-    return render_template('search.html', data=data)
+    return courts
 
-@app.route("/")
-def index():
-    db = pymongo.MongoClient(os.environ['MONGO_URI'])['court-search-temp']
-    prev_searches = list(db['searches'].find())
-    for search in prev_searches:
-        search['courts_searched'] = db['cases'].find({'name': search['name']}).count()
-    return render_template('index.html', searches=prev_searches)
+total_searches = 0
 
-if __name__ == "__main__":
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.secret_key = 'doesnt-need-to-be-secret'
-    app.run(host='0.0.0.0', port=port, debug=True)
+# get started
+name = sys.argv[1].upper()
+courts = start()
+court_full_names = [c['fullName'] for c in courts]
+
+# do first level search
+db = pymongo.MongoClient(os.environ['MONGO_URI'])['court-search-temp']
+courts_searched = [c['court'] for c in db['cases'].find({'name': name.upper()})]
+courts_to_search = set(court_full_names) - set(courts_searched)
+for court in courts_to_search:
+    print court
+    searchCourt(name, court)
+    total_searches += 1
+    if total_searches > 100:
+        total_searches = 0
+        start()
+
+# do second level search
+# look at first level and find names to search second level
+names_to_search = set()
+searches = db['cases'].find({'name': name.upper()})
+for search in searches:
+    for case in search['civilCases']:
+        plaintiff_name = case['Plaintiff']
+        if plaintiff_name.startswith(name): continue
+        if 'COUNTY OF' in plaintiff_name: continue
+        if 'CITY OF' in plaintiff_name: continue
+        plaintiff_name = plaintiff_name.replace(';',' ')
+        plaintiff_name_parts = plaintiff_name.split(' ')
+        if len(plaintiff_name_parts) < 2 or not plaintiff_name_parts[0].endswith(','): continue
+        plaintiff_name = ' '.join(plaintiff_name_parts[:2])
+        names_to_search.add(plaintiff_name)
+# search the names
+for court in court_full_names:
+    # create a list of names we have already searched
+    names_searched = set([c['name'] for c in db['cases'].find({'court': court}, {'name': True})])
+    names_to_search_this_court = names_to_search - names_searched
+    count_remaining = len(names_to_search_this_court)
+    for name in names_to_search_this_court:
+        print court, name, str(count_remaining), 'left'
+        count_remaining -= 1
+        searchCourt(name, court)
+        total_searches += 1
+        if total_searches > 100:
+            total_searches = 0
+            start()
