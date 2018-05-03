49aab312470199b67e7740a1620f44f78bd1c206
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2018-05-03 09:22:37 -0500

    Turn off debug mode

--- courts.py
+++ courts.py
@@ -390,4 +390,4 @@ if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.secret_key = 'doesnt-need-to-be-secret'
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
